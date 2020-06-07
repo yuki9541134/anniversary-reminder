08510e538bbc24a24e79c493fcf9f0e94144eb44
Yukiya Adachi <yadachi@YukiyanoMacBook-Pro.local>
2020-06-07 17:10:53 +0900

    relationにenumを使用する

--- src/Controller/PreciousUsersController.php
+++ src/Controller/PreciousUsersController.php
@@ -1,12 +1,18 @@
 <?php
 namespace App\Controller;
 
+require dirname(__FILE__) .'/../Lib/Enum.php';
+use Enum\Relation;
+
 class PreciousUsersController extends AppController
 {
     public function index()
     {
         $this->loadComponent('Paginator');
         $precious_users = $this->Paginator->paginate($this->PreciousUsers->find());
+        foreach ($precious_users as $precious_user) {
+            $precious_user->relation = Relation::search(0);
+        }
         $this->set(compact('precious_users'));
     }
 }
---
+++ src/Lib/Enum.php
@@ -0,0 +1,10 @@
+<?php
+namespace Enum; 
+
+use MyCLabs\Enum\Enum;
+
+class Relation extends Enum
+{
+    private const father = 0;
+    private const mother = 1;
+}
